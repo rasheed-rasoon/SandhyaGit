0abaf5a5167315431813fdcb3f8f35d8ce9166ef
Rasheed Ahmed <rasheedsheik66@gmail.com>
2019-02-25 12:15:27 +0530

    my first commit sandhya

---
+++ SandhyaGitProj/src/test/java/SandhyaGitProj/SandhyaGitProj/GitPractice.java
@@ -0,0 +1,10 @@
+package SandhyaGitProj.SandhyaGitProj;
+
+public class GitPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("my first commit");
+	}
+
+}
