854d99d8abc52efcd656c1baf98a75c3c49ab447
Rasheed Ahmed <rasheedsheik66@gmail.com>
2019-02-26 21:19:47 +0530

    deleted click on GHMC Officer link

--- JenkinsMavenGitHubProj/src/test/java/JenkinsMavenGitHub/JenkinsMavenGitHubProj/JenkinsMavenGit.java
+++ JenkinsMavenGitHubProj/src/test/java/JenkinsMavenGitHub/JenkinsMavenGitHubProj/JenkinsMavenGit.java
@@ -48,14 +48,14 @@ public void jenkinsInteg()
 	driver =new FirefoxDriver();
 	System.out.println("navigating to BRS URL");
 	driver.get("http://inctest.apcgg.gov.in");
-	System.out.println("waiting for 2 seconds");
-	try {
-		Thread.sleep(2000);
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	driver.findElement(By.id("LinkButton1")).click();
+//	System.out.println("waiting for 2 seconds");
+//	try {
+//		Thread.sleep(2000);
+//	} catch (InterruptedException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+//	driver.findElement(By.id("LinkButton1")).click();
+//}
 }
-
 }
