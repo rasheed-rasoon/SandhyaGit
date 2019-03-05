e7b6e3d54596d9e093130ae940046b486338104c
Rasheed Ahmed <rasheedsheik66@gmail.com>
2019-03-05 11:19:17 +0530

    05-03-2019 at 11:19

--- JenkinsMavenGitHubProj/src/test/java/rough/CollectionPrograms.java
+++ JenkinsMavenGitHubProj/src/test/java/rough/CollectionPrograms.java
@@ -2,12 +2,11 @@ package rough;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-
-import org.testng.annotations.Test;
+import java.util.ListIterator;
 
 public class CollectionPrograms {
-@Test
-public static void arryList()
+
+public static void arryListExample()
 {
 ArrayList<String> aList=new ArrayList<String>();
 aList.add("Rasheed");
@@ -21,15 +20,39 @@ for(String eachValue:aList)
 	
 System.out.println(eachValue);
 }
-
+   
 Iterator<String> itr=aList.iterator();
 
 System.out.println("traversing  using iterator ");
 while(itr.hasNext())
 {
 System.out.println(itr.next());	
+}	
+
+
+System.out.println("Traversing using List iterator in forward direction");
+ListIterator<String> listItr=aList.listIterator();
+
+while(listItr.hasNext())
+{
+System.out.println(listItr.next());	
+
 }
-	
-	
+System.out.println("Traversing the list in reverse direction");
+ListIterator<String> listItrBack=aList.listIterator(aList.size());
+
+while(listItrBack.hasPrevious())
+{
+System.out.println(listItrBack.previous());	
+
+}
+}
+
+
+public static void main(String[] args)
+{
+	CollectionPrograms.arryListExample();
+
+
 }
 }
